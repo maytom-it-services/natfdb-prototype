e8314654220d75d043de1b7cf7106109be68481f
Tony Maytom <tony.maytom@maytomit.com>
2018-08-15 20:59:43 +1000

    Add files via upload
    From laptop, updated with express

--- natfdb-prototype.js
+++ natfdb-prototype.js
@@ -1,8 +1,41 @@
-var http = require('http');
-var bs = require('bootstrap');
-var mongo = require('mongodb');
+var express = require("express");
+var https = require("https")
+var app = express();
+var router = express.Router();
 
-http.createServer(function(req,res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello world!');
-}).listen(8080);
+app.use(express.static("public"));
+app.use(express.static("data"));
+router.use(function (req,res,next) {
+    console.log("/" + req.method);
+    next();
+  });
+var path = __dirname + '/public/';
+router.get("/",function(req,res){
+    res.sendFile(path + "index.html");
+  });
+  
+  router.get("/add",function(req,res){
+    res.sendFile(path + "add-encounter.html");
+  });
+  
+  router.get("/history",function(req,res){
+    res.sendFile(path + "history-encounter.html");
+  });
+  
+  router.get("/profile",function(req,res){
+    res.sendFile(path + "profile.html");
+  });
+
+  router.get("/contact",function(req,res){
+    res.sendFile(path + "contact.html");
+  });
+
+  app.use("/",router);
+  
+  app.use("*",function(req,res){
+    res.sendFile(path + "404.html");
+  });
+  
+  app.listen(3000,function(){
+    console.log("Live at Port 3000");
+  });
